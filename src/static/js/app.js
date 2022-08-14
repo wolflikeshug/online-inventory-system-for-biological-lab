8d35293d11a780f3b52de90732932953dc7006c0
joe-potato <82947199+joe-potato@users.noreply.github.com>
2022-08-14 10:58:14 +0800

    Committing Simon's layout change, adjusted readme, edited js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -26,7 +26,7 @@ search.addEventListener("click", () => {
 function moveActiveTab() {
   let topPosition = activeIndex * 58 + 2.5;
 
-  if (activeIndex > 3) {
+  if (activeIndex > 2) {
     topPosition += shortcuts.clientHeight;
   }
 
