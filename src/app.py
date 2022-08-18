187c0647f94ef44f5811b3e5988cc0ac54ce981d
Joe <22982055@student.uwa.edu.au>
2022-08-18 17:56:49 +0800

    revised installation of virtual env., started web traversal

--- src/app.py
+++ src/app.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("dashboard.html")
+
+@app.route('/rooms')
+def rooms():
+    return render_template("rooms.html")
+
+@app.route('/freezers')
+def freezers():
+    return render_template("freezers.html")
+
+@app.route('/samples')
+def samples():
+    return render_template("samples.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
