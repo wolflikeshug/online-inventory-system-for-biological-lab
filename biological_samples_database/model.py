a309e7727c80d39d9362abc21d86794cba9b16b5
RunSeven <5661180+RunSeven@users.noreply.github.com>
2022-08-21 15:15:24 +0800

    Sample models

--- biological_samples_database/model.py
+++ biological_samples_database/model.py
@@ -43,20 +43,6 @@ class Sample(Base):
     notes = Column('notes', String)
 
 
-class Plasma(Sample):
-    """ORM Model for the Plasma table."""
-    __tablename__ = 'plasma'
-
-    # Overridden Variables
-    sample_date = Column('sample_date', Date, nullable=False)
-    sample_id = Column('primary_id', String, nullable=False)
-
-    # New Variables
-    visit_number = Column('visit_number', String)
-    volume_ml = Column('volume_ml', Integer)
-    notes = Column('notes', String)
-
-
 class CellLine(Sample):
     """ORM Model for the Cell Line table."""
     __tablename__ = 'cell_line'
@@ -67,3 +53,81 @@ class CellLine(Sample):
     growth_media = Column('growth_media', String)
     vial_source = Column('vial_source', String)
     lot_number = Column('lot_number', String)
+
+    def serialize(self):
+        """Create an object that can be handled as a JSON"""
+
+        return {
+            'id': self.id,
+            'sample_id': self.sample_id,
+            'sample_date': self.sample_date,
+            'volume_ml': self.volume_ml,
+            'sample_processor': self.sample_processor,
+            'notes': self.notes,
+            'cell_type': self.cell_type,
+            'passage_number': self.passage_number,
+            'cell_count': self.cell_count,
+            'growth_media': self.growth_media,
+            'vial_source': self.vial_source,
+            'lot_number': self.lot_number
+        }
+
+
+class Mosquito(Sample):
+    """ORM Model for the Mosquito table."""
+    __tablename__ = 'mosquito'
+
+    # Overridden Variables
+    sample_id = Column('primary_id', String, nullable=False)
+
+
+class Pbmc(Sample):
+    """ORM Model for the PBMC table."""
+    __tablename__ = 'pbmc'
+
+    # Overridden Variables
+    sample_date = Column('sample_date', Date, nullable=False)
+    sample_id = Column('primary_id', String, nullable=False)
+
+
+class Plasma(Sample):
+    """ORM Model for the Plasma table."""
+    __tablename__ = 'plasma'
+
+    # Overridden Variables
+    sample_date = Column('sample_date', Date, nullable=False)
+    sample_id = Column('primary_id', String, nullable=False)
+
+    # New Variables
+    visit_number = Column('visit_number', String)
+
+
+class Serum(Sample):
+    """ORM Model for the Serum table."""
+    __tablename__ = 'serum'
+
+
+class VirusCulture(Sample):
+    """ORM Model for the Virus Culture table."""
+    __tablename__ = 'virus_culture'
+
+    # Overridden Variables
+    sample_date = Column('sample_date', Date, nullable=False)
+
+    # New Variables
+    batch_number = Column('batch_number', String)
+    passage_number = Column('passage_number', Integer)
+    growth_media = Column('growth_media', String)
+
+
+class VirusIsolation(Sample):
+    """ORM Model for the Virus Isolation table."""
+    __tablename__ = 'virus_isolation'
+
+    # Overridden Variables
+    sample_date = Column('sample_date', Date, nullable=False)
+
+    # New Variables
+    batch_number = Column('batch_number', String)
+    passage_number = Column('passage_number', Integer, nullable=False)
+    growth_media = Column('growth_media', String)
