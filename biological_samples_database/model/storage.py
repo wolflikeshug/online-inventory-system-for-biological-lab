0a6f7d32dbfc42a3110e3704cd0239ca5d7e118d
RunSeven <5661180+RunSeven@users.noreply.github.com>
2022-08-28 06:50:15 +0800

    box, freezer, building models

--- biological_samples_database/model/storage.py
+++ biological_samples_database/model/storage.py
@@ -5,12 +5,9 @@ Holds the database ORM structures for SQL Alchemy
 
 """
 
-import uuid
+from sqlalchemy import Column, ForeignKey, String
 
-from sqlalchemy import Column, String  # , Table
-from sqlalchemy.orm import backref, relationship
-
-from ..model import Base
+from . import create_uuid, Base
 
 
 class Box(Base):
@@ -18,5 +15,26 @@ class Box(Base):
 
     __tablename__ = "box"
 
-    id = Column('id', String, primary_key=True, default=uuid.uuid4())
+    id = Column('id', String, primary_key=True, default=create_uuid())
+    label = Column('label', String, unique=True)
+    freezer_id = Column('freezer_id', ForeignKey('freezer.id'))
+    owner = Column('owner', String)
 
+
+class Freezer(Base):
+    """ORM Model for the freezer that boxes are stored in."""
+
+    __tablename__ = "freezer"
+
+    id = Column('id', String, primary_key=True, default=create_uuid())
+    name = Column('name', String, unique=True)
+    building_id = Column('building_id', ForeignKey('building.id'))
+
+
+class Building(Base):
+    """ORM Model for the building that freezer is located in."""
+
+    __tablename__ = "building"
+
+    id = Column('id', String, primary_key=True, default=create_uuid())
+    name = Column('name', String, unique=True)
