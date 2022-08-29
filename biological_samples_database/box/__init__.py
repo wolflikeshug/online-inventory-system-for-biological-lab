59a570a5652e08ed269c036fc9a0a2957159dc65
RunSeven <5661180+RunSeven@users.noreply.github.com>
2022-08-29 09:40:02 +0800

    box __init__ included

---
+++ biological_samples_database/box/__init__.py
@@ -0,0 +1,94 @@
+# Standard Imports
+import datetime
+import os
+import random
+import uuid
+
+# Flask
+from flask import Blueprint, jsonify, redirect, render_template, request
+
+# Flask WTF
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, HiddenField
+from wtforms import SelectField, StringField
+from wtforms.validators import InputRequired
+
+# Local Imports
+from ..database import create_new_session, engine, SQLITE_PATH
+from ..model.storage import Box
+
+
+BOX = Blueprint(
+    'box',
+    __name__,
+    template_folder='templates'
+)
+
+
+# Forms
+class BoxForm(FlaskForm):
+    """WTF for creating a new box"""
+
+    id = HiddenField('Id', [InputRequired()])
+    label = StringField('Name', [InputRequired()])
+    freezer_id = SelectField('Freezer', [InputRequired()])
+    owner = StringField('Owner', [])
+
+
+@BOX.route('/', methods=['POST'])
+def new_box():
+    """Insert a single dataset into the SQLite database"""
+
+    box = Box()
+    box.label = request.form.get('label')    
+    box.freezer_id = request.form.get('freezer_id')
+    box.owner = request.form.get('owner')
+
+    session = create_new_session()
+    session.add(
+        box
+    )
+
+    session.commit()
+    
+    return redirect(request.referrer)
+
+
+@BOX.route('/', methods=['GET'])
+def all_boxes():
+    """Retrieve all boxes"""
+
+    session = create_new_session()
+
+    boxes = session.query(
+        Box
+    ).all()
+
+    return render_template(
+        'boxes.html',
+        boxes=boxes
+    )
+
+@BOX.route('/<freezer_id>', methods=['GET'])
+def freezer_boxes(freezer_id):
+    """Retrieve boxes in a specific boxes"""
+
+    session = create_new_session()
+
+    boxes = session.query(
+        Box
+    ).filter(
+        Box.freezer_id == freezer_id
+    ).all()
+
+    return render_template(
+        'room.html',
+        boxes=boxes
+    )
+
+@BOX.route('/create/', methods=['GET'])
+def create_box():
+    """Provide the HTML form for box creation"""
+
+    form = BoxForm()
+    return render_template('box_create.html', form=form)
