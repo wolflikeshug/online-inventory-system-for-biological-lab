1d1577accb9135652f442b4ce00cbc1d0eeb6d1b
ptaylo <110022494+ptaylo@users.noreply.github.com>
2022-08-30 20:19:47 +0800

    finish adding user database models/ page redirects

--- biological_samples_database/model/user.py
+++ biological_samples_database/model/user.py
@@ -5,20 +5,23 @@ User model for SqlAlchemy
 
 """
 from sqlalchemy import Column, Integer, String  # , Table
-from sqlalchemy.ext.declarative import declared_attr
+from flask_login import UserMixin
+from biological_samples_database import data, login_man
 
-from . import Base
+@login_man.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
-
-class User(Base):
+class User(data.Model, UserMixin):
     """User class for session authentication and login"""
-
     __tablename__ = "user"
-
-    id = Column('id', String, primary_key=True)
+    id = Column('id', Integer, primary_key=True)
     username = Column('username', String(20), unique=True, nullable=False)
     first = Column('first', String(20), nullable=False)
     last = Column('last', String(20), nullable=False)
     email = Column('email', String(50), unique=True, nullable=False)
     password = Column('password', String(20), nullable=False)
     gid = Column('gid', Integer, nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.first}', '{self.last}', '{self.username}')"
