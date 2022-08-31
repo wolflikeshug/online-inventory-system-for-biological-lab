e1c38fc4f3e4b2b47d0c13eee574e533c0c1d583
wolf_likes_hug <47602450+wolflikeshug@users.noreply.github.com>
2022-08-31 09:34:14 +0800

    Create automation.sh
    auto build up the project's suitable environment

---
+++ automation.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+sudo apt update
+sudo apt upgrade -y
+sudo apt install python3.10-venv -y
+sudo apt update
+sudo apt upgrade -y
+sudo apt install git python3.10 python3-pip -y
+python3 -m pip3 install --upgrade pip
+git clone https://github.com/wolflikeshug/CITS3200-repo.git ~/CITS3200-repo
+pip3 install -r ~/CITS3200-repo/requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
