b2026969eed5854d8bef01a2f2542e2c2fc01ec8
RunSeven <5661180+RunSeven@users.noreply.github.com>
2022-09-03 09:54:58 +0800

    sample base

--- biological_samples_database/__init__.py
+++ biological_samples_database/__init__.py
@@ -13,6 +13,7 @@ import uuid
 from flask import Flask, render_template
 
 # Blueprint Imports
+from .samples import SAMPLE
 from .samples.cell_line import CELL_LINE
 from .samples.serum import SERUM
 from .freezer import FREEZER
@@ -100,6 +101,7 @@ def initialise_app():
     app = APP
     app.secret_key = 'HUSHHUSHVERYSECRET'
     initialise_sqlite_database()
+    app.register_blueprint(SAMPLE, url_prefix='/samples')
     app.register_blueprint(CELL_LINE, url_prefix='/samples/cell_line')
     app.register_blueprint(SERUM, url_prefix='/samples/serum')
     app.register_blueprint(FREEZER, url_prefix='/freezer/')
