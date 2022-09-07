39ae81db5ecb486b3cf1b42d78c7284e79684e1f
wolf_likes_hug <47602450+wolflikeshug@users.noreply.github.com>
2022-09-07 11:46:52 +0800

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -8,3 +8,4 @@ sudo apt install git python3.10 python3-pip -y
 python3 -m pip3 install --upgrade pip
 git clone https://github.com/wolflikeshug/CITS3200-repo.git ~/CITS3200-repo
 pip3 install -r ~/CITS3200-repo/requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+python3 wsgi.py
