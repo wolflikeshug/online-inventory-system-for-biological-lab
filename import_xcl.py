e3705346d143dc63e7c6acf76abeb8adffd09850
Bowles777 <65882946+Bowles777@users.noreply.github.com>
2022-09-15 18:49:27 +0800

    added import into the box table and the vials table

--- import_xcl.py
+++ import_xcl.py
@@ -25,13 +25,71 @@ second we want to add it to every sample under box id
 8 - excel.length we take this column by column and add them to the vials table by colnum. 
 Will also need to do an if statement on the second col (Sample type) so we add it to the specific type table
 
+
+database fixes = type of box and also what freezer its in to be discussed
+
 '''
 
-import pandas as pd 
+from biological_samples_database.model.sample import Vial
+from biological_samples_database.model.storage import Box 
+from biological_samples_database.database import create_new_session, engine
 
-df = pd.read_excel('Book1.xlsx')
+import openpyxl
 
-df 
+from sqlalchemy.orm import sessionmaker
+ 
+dataframe = openpyxl.load_workbook("Book1.xlsx")
+dataframe1 = dataframe.active
+
+sess = sessionmaker()
+sess.configure(bind=engine)
 
 
 
+box_table = []
+for row in range(0, 6):
+    for col in dataframe1.iter_cols(2, 2):
+        #print(col[row].value)
+        box_table.append(col[row].value)
+
+#print(box_table)
+
+box = Box()
+
+box.label = box_table[2]
+box.freezer_id = box_table[3]
+box.owner = box_table[5]
+box.id = box_table[0]
+box_sess = sess()
+box_sess.add(box)
+box_sess.commit()
+        
+
+def add_vials():
+    vial_table = []
+    
+    
+    for row in range(8, 18):
+        next = []
+        Sess = sess()
+        vials = Vial()
+        for col in dataframe1.iter_cols(1, dataframe1.max_column):
+            #print(col[row].value)
+            next.append(col[row].value)
+        vial_table.append(next)
+        #new_tab = []
+        #new_tab.append(next[0])
+
+        vials.position = next[0]
+        vials.box_id = box_table[0]
+        vials.lab_id = next[3]
+        vials.sample_date = next[5]
+        vials.volume_ml = next[13]
+        vials.user_id = next[15]
+        vials.notes = next[16]
+
+        Sess.add(vials)
+        Sess.commit()
+    #print(vial_table)
+
+add_vials()
