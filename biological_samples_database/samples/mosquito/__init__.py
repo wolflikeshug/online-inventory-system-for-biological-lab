7fce74cf23fc88be78785c5e1afc30e110dd71e9
RunSeven <5661180+RunSeven@users.noreply.github.com>
2022-09-17 12:49:54 +0800

    mosquito

---
+++ biological_samples_database/samples/mosquito/__init__.py
@@ -0,0 +1,92 @@
+"""
+
+Mosquito
+
+All API information related to Mosquito samples
+
+"""
+
+# Flask
+from flask import Blueprint, redirect, render_template, request
+
+# Flask WTF
+from wtforms import StringField
+
+# Local Imports
+from .. import SampleForm, populate_default_values
+from ...database import create_new_session
+from ...model.sample import Mosquito
+from ...model.storage import Box
+
+
+MOSQUITO = Blueprint(
+    'mosquito',
+    __name__,
+    template_folder='templates'
+)
+
+
+class MosquitoForm(SampleForm):
+    '''Form for handling Mosquito data'''
+
+    pathwest_id = StringField('PathWest ID')
+
+
+@MOSQUITO.route('/', methods=['POST'])
+def create():
+    """Insert a single dataset into the SQLite database"""
+
+    mosquito = Mosquito()
+    populate_default_values(request, mosquito)
+
+    with create_new_session() as session:
+
+        session.add(
+            mosquito
+        )
+
+        session.commit()
+        return redirect(request.referrer)
+
+
+@MOSQUITO.route('/', methods=['GET'])
+def read_all():
+    """Placeholder for retrieving Mosquito data from the SQLite database"""
+
+    form = MosquitoForm()
+
+    with create_new_session() as session:
+
+        mosquitos = session.query(
+            Mosquito
+        ).all()
+
+        print(mosquitos)
+
+        return render_template(
+            'mosquito.html',
+            mosquitos=mosquitos,
+            form=form
+        )
+
+
+@MOSQUITO.route('/create/', methods=['GET'])
+def create_mosquito():
+    """Provide the HTML form for mosquito creation"""
+
+    sample_title = 'Add Mosquito'
+    sample_action = "/samples/mosquito/"
+
+    with create_new_session() as session:
+
+        boxes = session.query(
+            Box
+        ).all()
+
+        form = MosquitoForm()
+        return render_template(
+            'mosquito_create.html',
+            form=form,
+            boxes=boxes,
+            sample_title=sample_title,
+            sample_action=sample_action)
