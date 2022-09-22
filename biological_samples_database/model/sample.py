fc69a9fe4b2902cae2aea0d853ebdfa13f5cb4a7
wolf_likes_hug <47602450+wolflikeshug@users.noreply.github.com>
2022-09-23 04:17:26 +0800

    update sample.py create search folder

--- biological_samples_database/model/sample.py
+++ biological_samples_database/model/sample.py
@@ -6,7 +6,7 @@ Holds vial sample structures for various sample types
 
 """
 
-from datetime import datetime
+from datetime import datetime, date
 
 from sqlalchemy import (
     Boolean,
@@ -26,189 +26,215 @@ class Sample(Base):
 
     __tablename__ = "sample"
 
-    id = Column('id', String, primary_key=True, default=generate_uuid)
-    lab_id = Column('lab_id', String, unique=True)
-    notes = Column('notes', String)
+    id      = Column('id', String, primary_key=True, default=generate_uuid)
+    lab_id  = Column('lab_id', String, unique=True)
+    notes   = Column('notes', String)
 
 
 class Vial(Base):
     """Master table holding baseline data for all vials in the database."""
     __tablename__ = 'vial'
 
-    id = Column(
-        'id',
-        String,
-        primary_key=True,
-        default=generate_uuid)
-    lab_id = Column('lab_id', String, default='UNKNOWN')
-    box_id = Column('box_id', ForeignKey('box.id'))
-    position = Column('position', String, default='UNKNOWN')
-    sample_date = Column(
-        'sample_date',
-        Date,
-        default=datetime.strptime('01-01-1900', '%d-%M-%Y'))
-    volume_ml = Column('volume_ml', Float, default=-9999)
-    user_id = Column('user_id', String, default='UNKKNOWN')
-    notes = Column('notes', String)
-    used = Column('used', Boolean, default=False)
+    id          = Column('id', String, primary_key=True, default=generate_uuid)
+    lab_id      = Column('lab_id', String, default='UNKNOWN')
+    box_id      = Column('box_id', ForeignKey('box.id'))
+    position    = Column('position', String, default='UNKNOWN')
+    sample_date = Column('sample_date', Date, default=datetime.strptime('01-01-1900', '%d-%M-%Y'))
+    volume_ml   = Column('volume_ml', Float, default=-1.0)
+    user_id     = Column('user_id', String, default='UNKKNOWN')
+    notes       = Column('notes', String)
+    used        = Column('used', Boolean, default=False)
     sample_type = Column('sample_type', String)
+    initials    = Column('initials', String, default='UNKNOWN')
+    other       = Column('other', String, default='UNKNOWN')
 
     __mapper_args__ = {
         'polymorphic_identity': 'vial',
         'polymorphic_on': sample_type
     }
 
-
-class Antigen(Vial):
-    """ORM Model for the Antigen table."""
-    __tablename__ = 'antigen'
-
-    id = Column(
-        'id',
-        ForeignKey('vial.id'),
-        primary_key=True,
-        default=generate_uuid)
-
-    # New Variables
-    pathwest_id = Column('pathwest_id', String, default='UNKNOWN')
-    batch_number = Column('batch_number', Integer, default=-9999)
-    passage_number = Column('passage_number', Integer, default=-9999)
-    growth_media = Column('growth_media', String, default='UNKNOWN')
-
-    __mapper_args__ = {
-        'polymorphic_identity': 'antigen'
-    }
-
-
-class CellLine(Vial):
-    """ORM Model for the Cell Line table."""
-    __tablename__ = 'cell_line'
-
-    id = Column(
-        'id',
-        ForeignKey('vial.id'),
-        primary_key=True,
-        default=generate_uuid)
-    cell_type = Column('cell_type', String, default='UNKNOWN', nullable=False)
-    passage_number = Column('passage_number', Integer, default=-9999)
-    cell_count = Column('cell_count', Integer, default=-9999)
-    growth_media = Column('growth_media', String, default='UNKNOWN')
-    vial_source = Column('vial_source', String, default='UNKNOWN')
-    lot_number = Column('lot_number', String, default='UNKNOWN')
-
-    __mapper_args__ = {
-        'polymorphic_identity': 'cell_line'
-    }
-
-
-class Mosquito(Vial):
-    """ORM Model for the Mosquito table."""
-    __tablename__ = 'mosquito'
-
-    id = Column(
-        'id',
-        ForeignKey('vial.id'),
-        primary_key=True,
-        default=generate_uuid)
-
-    __mapper_args__ = {
-        'polymorphic_identity': 'mosquito'
-    }
-
-
-class Pbmc(Vial):
-    """ORM Model for the PBMC table."""
-    __tablename__ = 'pbmc'
-
-    id = Column(
-        'id',
-        ForeignKey('vial.id'),
-        primary_key=True,
-        default=generate_uuid)
-
-    # New Variables
-    visit_number = Column('visit_number', Integer, default=-9999)
-    cell_count = Column('cell_count', Integer, default=-9999)
-    patient_code = Column('patient_code', String)
-
-    __mapper_args__ = {
-        'polymorphic_identity': 'pbmc'
-    }
-
-
-class Plasma(Vial):
-    """ORM Model for the Plasma table."""
-    __tablename__ = 'plasma'
-
-    id = Column(
-        'id',
-        ForeignKey('vial.id'),
-        primary_key=True,
-        default=generate_uuid)
-
-    # New Variables
-    visit_number = Column('visit_number', Integer, default=-9999)
-
-    __mapper_args__ = {
-        'polymorphic_identity': 'plasma'
-    }
-
-
 class Serum(Vial):
     """ORM Model for the Serum table."""
     __tablename__ = 'serum'
 
-    id = Column(
-        'id',
-        ForeignKey('vial.id'),
-        primary_key=True,
-        default=generate_uuid)
+    id              = Column('id', ForeignKey('vial.id'), primary_key=True, default=generate_uuid)
 
     # New Variables
-    pathwest_id = Column('pathwest_id', String, default='UNKNOWN')
-
-    __mapper_args__ = {
+    pathwest_id     = Column('pathwest_id', String, default='UNKNOWN')
+    
+    __mapper_args__ = { 
         'polymorphic_identity': 'serum'
     }
-
-
-class VirusCulture(Vial):
-    """ORM Model for the Virus Culture table."""
-    __tablename__ = 'virus_culture'
-
-    id = Column(
-        'id',
-        ForeignKey('vial.id'),
-        primary_key=True,
-        default=generate_uuid)
-
-    # New Variables
-    pathwest_id = Column('pathwest_id', String, default='UNKNOWN')
-    batch_number = Column('batch_number', Integer, default=-9999)
-    passage_number = Column('passage_number', Integer, default=-9999)
-    growth_media = Column('growth_media', String, default='UNKNOWN')
-
-    __mapper_args__ = {
-        'polymorphic_identity': 'virus_culture'
-    }
-
 
 class VirusIsolation(Vial):
     """ORM Model for the Virus Isolation table."""
     __tablename__ = 'virus_isolation'
 
-    id = Column(
-        'id',
-        ForeignKey('vial.id'),
-        primary_key=True,
-        default=generate_uuid)
+    id              = Column('id', ForeignKey('vial.id'), primary_key=True, default=generate_uuid)
 
     # New Variables
-    pathwest_id = Column('pathwest_id', String, default='UNKNOWN')
-    batch_number = Column('batch_number', Integer, default=-9999)
-    passage_number = Column('passage_number', Integer, default=-9999)
-    growth_media = Column('growth_media', String, default='UNKNOWN')
+    pathwest_id     = Column('pathwest_id', String, default='UNKNOWN')
+    batch_number    = Column('batch_number', Integer, default=-1)
+    passage_number  = Column('passage_number', Integer, default=-1)
+    growth_media    = Column('growth_media', String, default='UNKNOWN')
 
     __mapper_args__ = {
         'polymorphic_identity': 'virus_isolation'
     }
+
+class VirusCulture(Vial):
+    """ORM Model for the Virus Culture table."""
+    __tablename__ = 'virus_culture'
+
+    id              = Column('id', ForeignKey('vial.id'), primary_key=True, default=generate_uuid)
+
+    # New Variables
+    pathwest_id     = Column('pathwest_id', String, default='UNKNOWN')
+    batch_number    = Column('batch_number', Integer, default=-1)
+    passage_number  = Column('passage_number', Integer, default=-1)
+    growth_media    = Column('growth_media', String, default='UNKNOWN')
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'virus_culture'
+    }
+
+class Plasma(Vial):
+    """ORM Model for the Plasma table."""
+    __tablename__ = 'plasma'
+
+    id              = Column('id', ForeignKey('vial.id'), primary_key=True, default=generate_uuid)
+
+    # New Variables
+    
+    visit_number    = Column('visit_number', Integer, default=-1)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'plasma'
+    }
+
+class Pbmc(Vial):
+    """ORM Model for the PBMC table."""
+    __tablename__ = 'pbmc'
+
+    id              = Column('id', ForeignKey('vial.id'), primary_key=True, default=generate_uuid)
+
+    # New Variables
+    visit_number    = Column('visit_number', Integer, default=-1)
+    cell_count      = Column('cell_count', Integer, default=-1)
+    patient_code    = Column('patient_code', String, default='UNKNOWN')
+    
+    __mapper_args__ = {
+        'polymorphic_identity': 'pbmc'
+    }
+
+class CellLine(Vial):
+    """ORM Model for the Cell Line table."""
+    __tablename__ = 'cell_line'
+
+    id              = Column('id', ForeignKey('vial.id'), primary_key=True, default=generate_uuid)
+    cell_type       = Column('cell_type', String, default='UNKNOWN', nullable=False)
+    passage_number  = Column('passage_number', Integer, default=-1)
+    cell_count      = Column('cell_count', Integer, default=-1)
+    growth_media    = Column('growth_media', String, default='UNKNOWN')
+    vial_source     = Column('vial_source', String, default='UNKNOWN')
+    lot_number      = Column('lot_number', String, default='UNKNOWN')
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'cell_line'
+    }
+
+class Mosquito(Vial):
+    """ORM Model for the Mosquito table."""
+    __tablename__   = 'mosquito'
+
+    id              = Column('id', ForeignKey('vial.id'), primary_key=True, default=generate_uuid)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'mosquito'
+    }
+
+class Antigen(Vial):
+    """ORM Model for the Antigen table."""
+    __tablename__   = 'antigen'
+
+    id              = Column('id', ForeignKey('vial.id'), primary_key=True, default=generate_uuid)
+
+    # New Variables
+    pathwest_id     = Column('pathwest_id', String, default='UNKNOWN')
+    batch_number    = Column('batch_number', Integer, default=-1)
+    lot_number      = Column('lot_number', String, default='UNKNOWN')
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'antigen'
+    }
+
+class RNA (Vial):
+    """ORM Model for the RNA table."""
+    __tablename__   = "rna"
+
+    id              = Column('id', ForeignKey('vial.id'), primary_key=True, default=generate_uuid)
+
+    pathwest_id     = Column('pathwest_id', String, default='UNKNOWN')
+    batch_number    = Column('batch_number', Integer, default=-1)
+    lot_number      = Column('lot_number', String, default='UNKNOWN')
+
+    __mapper_args__ = { 
+        'polymorphic_identity': 'rna'
+    }
+
+class Peptide (Vial):
+    """ORM Model for the Peptide table."""
+    __tablename__   = "peptide"
+
+    id              = Column('id', ForeignKey('vial.id'), primary_key=True, default=generate_uuid)
+    
+    cell_type       = Column('cell_type', String, default='-')
+    batch_number    = Column('batch_number', Integer, default=-1)
+    vial_source     = Column('vial_source', String, default='UNKNOWN')
+    lot_number      = Column('lot_number', String, default='UNKNOWN')
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'peptide'
+    }
+
+class Supernatant (Vial):
+    """ORM Model for the Supernatant table."""
+    __tablename__   = "supernatant"
+
+    id              = Column('id', ForeignKey('vial.id'), primary_key=True, default=generate_uuid)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'supernatant'
+    }
+
+class Other (Vial):
+    """ORM Model for the Other table."""
+    __tablename__   = "other"
+
+    id              = Column('id', ForeignKey('vial.id'), primary_key=True, default=generate_uuid)
+    
+    __mapper_args__ = {
+        'polymorphic_identity': 'other'
+    }
+
+
+# Sorry  I need to add this into your sample file, for search purposes
+class Search_Result(Base):
+    """A class contains all the information of a search result."""
+    __tablename__   = 'search_result'
+    sample_type     = Column('sample_type', String, default='-')
+    pathwest_id     = Column('pathwest_id', String, default='-')
+    id              = Column('id', String, primary_key=True, default='-')
+    cell_type       = Column('cell_type', String, default='-')
+    sample_date     = Column('sample_date', Date, default=date(1900, 1, 1))
+    vist_number     = Column('vist_number', Integer, default=-1)
+    batch_number    = Column('batch_number', Integer, default=-1)
+    passage_number  = Column('passage_number', String, default='-')
+    cell_count      = Column('cell_count', Integer, default=-1)
+    growth_media    = Column('growth_media', String, default='-')
+    vial_source     = Column('vial_source', String, default='-')
+    volume_ml       = Column('volumn', Float, default=0.0)
+    patient_code    = Column('patient_code', String, default='-')
+    initials        = Column('initials', String, default='-')
+    other           = Column('other', String, default='-')
+
---
+++ biological_samples_database/search/__init__.py
@@ -0,0 +1,244 @@
+"""
+
+Cell Line
+
+All API information related to Cell Line samples
+
+"""
+# Standard
+from datetime import datetime
+
+# Flask
+from flask import Blueprint, redirect, render_template, request
+
+# Flask WTF
+from wtforms import IntegerField, StringField
+
+from flask_wtf import FlaskForm
+from wtforms import (
+    BooleanField,
+    DateField,
+    FloatField,
+    IntegerField,
+    StringField
+)
+
+from ..model.sample import Search_Result
+
+from .search_from_database import query_data_from_database
+
+SEARCH = Blueprint(
+    'search_output',
+    __name__,
+    template_folder='templates'
+)
+
+class SearchForm(FlaskForm):
+    '''Website link for page holding RSS data'''
+
+    Serum_sele = BooleanField('Serum', default=False)
+    Virus_Isolation_sele = BooleanField('Virus Isolation', default=False)
+    Virus_Culture_sele = BooleanField('Virus Culture', default=False)
+    Plasma_sele = BooleanField('Plasma', default=False)
+    PBMC_sele = BooleanField('PBMC', default=False)
+    Cell_Line_sele = BooleanField('Cell Line', default=False)
+    Mosquito_sele = BooleanField('Mosquito', default=False)
+    Antigen_sele = BooleanField('Antigen', default=False)
+    RNA_sele = BooleanField('RNA', default=False)
+    Peptide_sele = BooleanField('Peptide', default=False)
+    Supernatant_sele = BooleanField('Supernatant', default=False)
+    Other_sele = BooleanField('Other', default=False)
+
+    pw_id = StringField('PW_ID')
+    id = StringField('ID')
+    cell_type = StringField('Type')
+
+    start_date = DateField(
+        'Start Date',
+        default=datetime.strptime(
+            '2022-01-01',
+            '%Y-%m-%d'),
+        format='%Y-%m-%d'
+    )
+    end_date = DateField(
+        'End Date(This need to be later than the start date)',
+        default=datetime.strptime(
+            '2022-01-01',
+            '%Y-%m-%d'),
+        format='%Y-%m-%d'
+    )
+    need_date = BooleanField('Choose if you wish to search with a period of time', default=False)
+
+    visit_number = IntegerField('Visit Number')
+    batch_number = IntegerField('Batch Number')
+    passage_number = IntegerField('Passage Number')
+    cell_count = IntegerField('Total Count')
+    growth_media = StringField('Media')
+    vial_source = StringField('Source')
+    lot_number = StringField('Lot Number')
+    volume_ml = FloatField('Volume (ml)')
+    patient_code = StringField('Patient Code')
+    initials = StringField('Initials')
+    other = StringField('Other')
+
+search_input = [[], None, None, None, [None, None], None, None, None, None, None, None, None, None, None, None, None]
+
+@SEARCH.route('/', methods=['GET'])
+def read_all():
+    """Placeholder for retrieving Cell Line data from the SQLite database"""
+
+    form = SearchForm()
+
+    if request.form.get('Serum_sele') == 'y':
+        search_input[0].append('Serum')
+    if request.form.get('Virus_Isolation_sele') == 'y':
+        search_input[0].append('Virus Isolation')
+    if request.form.get('Virus_Culture_sele') == 'y':
+        search_input[0].append('Virus Culture')
+    if request.form.get('Plasma_sele') == 'y':
+        search_input[0].append('Plasma')
+    if request.form.get('PBMC_sele') == 'y':
+        search_input[0].append('PBMC')
+    if request.form.get('Cell_Line_sele') == 'y':
+        search_input[0].append('Cell Line')
+    if request.form.get('Mosquito_sele') == 'y':
+        search_input[0].append('Mosquito')
+    if request.form.get('Antigen_sele') == 'y':
+        search_input[0].append('Antigen')
+    if request.form.get('RNA_sele') == 'y':
+        search_input[0].append('RNA')
+    if request.form.get('Peptide_sele') == 'y':
+        search_input[0].append('Peptide')
+    if request.form.get('Supernatant_sele') == 'y':
+        search_input[0].append('Supernatant')
+    if request.form.get('Other_sele') == 'y':
+        search_input[0].append('Other')
+    
+    search_input[1] = request.form.get('pw_id')
+    search_input[2] = request.form.get('id')
+    search_input[3] = request.form.get('cell_type')
+    if request.form.get('need_date') == 'y':
+        search_input[4][0] = request.form.get('start_date')
+        search_input[4][1] = request.form.get('end_date')
+    search_input[5] = request.form.get('visit_number')
+    search_input[6] = request.form.get('batch_number')
+    search_input[7] = request.form.get('passage_number')
+    search_input[8] = request.form.get('cell_count')
+    search_input[9] = request.form.get('growth_media')
+    search_input[10] = request.form.get('vial_source')
+    search_input[11] = request.form.get('lot_number')
+    search_input[12] = request.form.get('volume_ml')
+    search_input[13] = request.form.get('patient_code')
+    search_input[14] = request.form.get('initials')
+    search_input[15] = request.form.get('other')
+
+    search_raw_output = query_data_from_database(search_input)
+    search_output = []
+    for i in range(1, len(search_raw_output[0])):
+        for n in range(0, len(search_raw_output[i])):
+            if search_raw_output[0][i] == "Serum":
+                search_output.append(Search_Result(pathwest_id = search_raw_output[i][n].pathwest_id, \
+                                                    id = search_raw_output[i][n].id, \
+                                                    sample_date = search_raw_output[i][n].sample_date, \
+                                                    volume_ml = search_raw_output[i][n].volume_ml, \
+                                                    initials = search_raw_output[i][n].initials, \
+                                                    other = search_raw_output[i][n].other))
+            elif search_raw_output[0][i] == "Virus Isolation":
+                search_output.append(Search_Result(pathwest_id = search_raw_output[i][n].pathwest_id, \
+                                                    id = search_raw_output[i][n].id,\
+                                                    sample_date = search_raw_output[i][n].sample_date, \
+                                                    batch_number = search_raw_output[i][n].batch_number, \
+                                                    passage_number = search_raw_output[i][n].passage_number, \
+                                                    growth_media = search_raw_output[i][n].growth_media, \
+                                                    volume_ml = search_raw_output[i][n].volume_ml, \
+                                                    initials = search_raw_output[i][n].initials, \
+                                                    other = search_raw_output[i][n].other))
+            elif search_raw_output[0][i] == "Virus Culture":
+                search_output.append(Search_Result(pathwest_id = search_raw_output[i][n].pathwest_id, \
+                                                    id = search_raw_output[i][n].id, sample_date = search_raw_output[i][n].sample_date, \
+                                                    batch_number = search_raw_output[i][n].batch_number, \
+                                                    passage_number = search_raw_output[i][n].passage_number, \
+                                                    growth_media = search_raw_output[i][n].growth_media, \
+                                                    volume_ml = search_raw_output[i][n].volume_ml, \
+                                                    initials = search_raw_output[i][n].initials, \
+                                                    other = search_raw_output[i][n].other))
+            elif search_raw_output[0][i] == "Plasma":
+                search_output.append(Search_Result(id = search_raw_output[i][n].id, \
+                                                    sample_date = search_raw_output[i][n].sample_date, \
+                                                    visit_number = search_raw_output[i][n].visit_number, \
+                                                    volume_ml = search_raw_output[i][n].volume_ml, \
+                                                    initials = search_raw_output[i][n].initials, \
+                                                    other = search_raw_output[i][n].other))
+            elif search_raw_output[0][i] == "PBMC":
+                search_output.append(Search_Result(id = search_raw_output[i][n].id, \
+                                                    sample_date = search_raw_output[i][n].sample_date, \
+                                                    visit_number = search_raw_output[i][n].visit_number, \
+                                                    volume_ml = search_raw_output[i][n].volume_ml, \
+                                                    patient_code = search_raw_output[i][n].patient_code, \
+                                                    initials = search_raw_output[i][n].initials, \
+                                                    other = search_raw_output[i][n].other))
+            elif search_raw_output[0][i] == "Cell Line":
+                search_output.append(Search_Result(id = search_raw_output[i][n].id, \
+                                                    cell_type = search_raw_output[i][n].cell_type, \
+                                                    sample_date = search_raw_output[i][n].sample_date, \
+                                                    passage_number = search_raw_output[i][n].passage_number, \
+                                                    cell_count = search_raw_output[i][n].cell_count, \
+                                                    growth_media = search_raw_output[i][n].growth_media, \
+                                                    lot_number = search_raw_output[i][n].lot_number, \
+                                                    volume_ml = search_raw_output[i][n].volume_ml, \
+                                                    initials = search_raw_output[i][n].initials, \
+                                                    other = search_raw_output[i][n].other))
+            elif search_raw_output[0][i] == "Mosquito":
+                search_output.append(Search_Result(id = search_raw_output[i][n].id, \
+                                                    sample_date = search_raw_output[i][n].sample_date, \
+                                                    volume_ml = search_raw_output[i][n].volume_ml, \
+                                                    initials = search_raw_output[i][n].initials, \
+                                                    other = search_raw_output[i][n].other))
+            elif search_raw_output[0][i] == "Antigen":
+                search_output.append(Search_Result(pathwest_id = search_raw_output[i][n].pathwest_id, \
+                                                    id = search_raw_output[i][n].id, \
+                                                    sample_date = search_raw_output[i][n].sample_date, \
+                                                    batch_number = search_raw_output[i][n].batch_number, \
+                                                    lot_number = search_raw_output[i][n].lot_number, \
+                                                    volume_ml = search_raw_output[i][n].volume_ml, \
+                                                    initials = search_raw_output[i][n].initials, \
+                                                    other = search_raw_output[i][n].other))
+            elif search_raw_output[0][i] == "RNA":
+                search_output.append(Search_Result(pathwest_id = search_raw_output[i][n].pathwest_id, \
+                                                    id = search_raw_output[i][n].id, \
+                                                    sample_date = search_raw_output[i][n].sample_date, \
+                                                    batch_number = search_raw_output[i][n].batch_number, \
+                                                    lot_number = search_raw_output[i][n].lot_number, \
+                                                    volume_ml = search_raw_output[i][n].volume_ml, \
+                                                    initials = search_raw_output[i][n].initials, \
+                                                    other = search_raw_output[i][n].other))
+            elif search_raw_output[0][i] == "Peptide":
+                search_output.append(Search_Result(id = search_raw_output[i][n].id, \
+                                                    cell_type = search_raw_output[i][n].cell_type, \
+                                                    sample_date = search_raw_output[i][n].sample_date, \
+                                                    batch_number = search_raw_output[i][n].batch_number, \
+                                                    vial_source = search_raw_output[i][n].vial_source, \
+                                                    lot_number = search_raw_output[i][n].lot_number, \
+                                                    volume_ml = search_raw_output[i][n].volume_ml, \
+                                                    initials = search_raw_output[i][n].initials, \
+                                                    other = search_raw_output[i][n].other))
+            elif search_raw_output[0][i] == "Supernatant":
+                search_output.append(Search_Result(id = search_raw_output[i][n].id, \
+                                                    sample_date = search_raw_output[i][n].sample_date, \
+                                                    volume_ml = search_raw_output[i][n].volume_ml, \
+                                                    initials = search_raw_output[i][n].initials, \
+                                                    other = search_raw_output[i][n].other))
+            elif search_raw_output[0][i] == "Other":
+                search_output.append(Search_Result(id = search_raw_output[i][n].id, \
+                                                    sample_date = search_raw_output[i][n].sample_date, \
+                                                    volume_ml = search_raw_output[i][n].volume_ml, \
+                                                    initials = search_raw_output[i][n].initials, \
+                                                    other = search_raw_output[i][n].other))
+    return render_template(
+        'search_base.html',
+        sample_type='search_result',
+        target_sample_header_html_file='search_header_stub.html',
+        target_sample_data_html_file='search_data_stub.html',
+        samples=search_output,
+        form=form
+    )
