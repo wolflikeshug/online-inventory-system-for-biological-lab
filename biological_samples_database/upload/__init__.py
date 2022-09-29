d746aa92a854ffd77d9bfb7ed566d27d42f3109a
ptaylo <110022494+ptaylo@users.noreply.github.com>
2022-09-29 11:32:40 +0800

    provide visual feedback of upload success

--- biological_samples_database/upload/__init__.py
+++ biological_samples_database/upload/__init__.py
@@ -7,11 +7,12 @@ User
 import os
 from werkzeug.utils import secure_filename
 # Flask
-from flask import Blueprint, render_template, current_app, redirect, request
+from flask import Blueprint, render_template, current_app, redirect, request, flash
 from ..forms import UploadFileForm
 # Local Imports
-
 from ..authentication import phd_required
+
+# Define Blueprint
 UPLOAD = Blueprint(
     'upload',
     __name__,
@@ -26,6 +27,7 @@ def home():
         file = form.file.data # First grab the file
         path = os.path.join(current_app.root_path,current_app.config['UPLOAD_FOLDER'],secure_filename(file.filename)) #Build path
         file.save(path)# Then save the file
+        flash(f'{file.filename} Uploaded', 'info')
         return redirect(request.referrer)
     return render_template('import.html', form=form)
 
